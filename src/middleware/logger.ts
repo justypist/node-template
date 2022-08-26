06109f3c6193470b3c4b20ead67832478f2caf7e
yongx <1003659191@qq.com>
2022-08-27 02:20:37 +0800

    feat: add logger

--- src/middleware/logger.ts
+++ src/middleware/logger.ts
@@ -1,6 +1,11 @@
+import { randomUUID } from 'crypto';
 import { Middleware } from 'koa';
 
 export const logger: Middleware = async (ctx, next) => {
-  console.log('ctx.url', ctx.url);
-  next();
+  const logId = randomUUID();
+  console.time(logId);
+  console.debug(ctx.request);
+  await next();
+  console.timeEnd(logId);
+  console.debug(ctx.response);
 };
