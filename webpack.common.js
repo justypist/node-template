ae39b22181854f4aae7fbdd3e1e8562ed8615e2a
谢勇 <xieyong.2021@bytedance.com>
2022-09-04 20:49:15 +0800

    chore: add node externals

--- webpack.common.js
+++ webpack.common.js
@@ -1,11 +1,18 @@
-const path = require('path');
+const path = require( 'path' );
+const nodeExternals = require( 'webpack-node-externals' );
 
 const WebpackConfig = {
   target: 'node',
+  externalsPresets: {
+    node: true,
+  },
+  externals: [
+    nodeExternals(),
+  ],
   entry: './src/index.ts',
   output: {
     filename: 'index.js',
-    path: path.resolve('./dist'),
+    path: path.resolve( './dist' ),
     clean: true,
   },
   module: {
@@ -15,22 +22,25 @@ const WebpackConfig = {
         use: [
           {
             loader: 'ts-loader',
-            options: {
-              transpileOnly: true,
-            },
+            // options: {
+            //   transpileOnly: true,
+            // },
           },
         ],
       },
     ],
   },
+  stats: {
+    errorDetails: true,
+  },
   resolve: {
-    extensions: ['.json', '.ts', '.js'],
+    extensions: [ '.json', '.ts', '.js' ],
     alias: {
-      '@controller': path.resolve('src', 'controller'),
-      '@service': path.resolve('src', 'service'),
-      '@entity': path.resolve('src', 'entity'),
-      '@util': path.resolve('src', 'util'),
-      '@middleware': path.resolve('src', 'middleware'),
+      '@controller': path.resolve( 'src', 'controller' ),
+      '@service': path.resolve( 'src', 'service' ),
+      '@entity': path.resolve( 'src', 'entity' ),
+      '@util': path.resolve( 'src', 'util' ),
+      '@middleware': path.resolve( 'src', 'middleware' ),
     },
   },
 };
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,21 +1,21 @@
-const path = require('path');
-const { merge } = require('webpack-merge');
-const common = require('./webpack.common');
-const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
-const NodemonWebpackPlugin = require('nodemon-webpack-plugin');
+const path = require( 'path' );
+const { merge } = require( 'webpack-merge' );
+const common = require( './webpack.common' );
+const ForkTsCheckerWebpackPlugin = require( 'fork-ts-checker-webpack-plugin' );
+const NodemonWebpackPlugin = require( 'nodemon-webpack-plugin' );
 
-module.exports = merge(common, {
+module.exports = merge( common, {
   mode: 'development',
   devtool: 'eval-source-map',
   plugins: [
-    new ForkTsCheckerWebpackPlugin(),
-    new NodemonWebpackPlugin({
+    // new ForkTsCheckerWebpackPlugin(),
+    new NodemonWebpackPlugin( {
       // If using more than one entry, you can specify
       // which output file will be restarted.
       script: './dist/index.js',
 
       // What to watch.
-      watch: path.resolve('./dist'),
+      watch: path.resolve( './dist' ),
 
       // Arguments to pass to the script being watched.
       args: [],
@@ -24,7 +24,7 @@ module.exports = merge(common, {
       nodeArgs: [],
 
       // Files to ignore.
-      ignore: ['*.js.map'],
+      ignore: [ '*.js.map' ],
 
       // Extensions to watch.
       ext: 'js,json',
@@ -40,6 +40,6 @@ module.exports = merge(common, {
       env: {
         NODE_ENV: 'development',
       },
-    }),
+    } ),
   ],
-});
+} );
