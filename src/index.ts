c9e9e70556deddd324ca5de68bda7e4ecf9b5aaa
typist <1003659191@qq.com>
2023-07-23 11:49:46 +0000

    refactor: remove koa

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,3 @@
-import 'module-alias/register';
-import 'reflect-metadata';
-
-import { router } from '@controller';
-import { database } from '@middleware/database';
-import { logger } from '@middleware/logger';
-import Koa from 'koa';
-import KoaBody from 'koa-body';
-
-const app = new Koa();
-
-app.use(KoaBody());
-app.use(logger);
-app.use(database);
-
-app.use(router.routes());
-
-app.listen(2000);
+(async () => {
+  console.log('node service started.');
+})();
